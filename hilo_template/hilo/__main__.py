31d0120be6e3993a1f439bf69ba864058cea4a82
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-01-20 14:31:36 -0700

    Update __main__.py

--- hilo_template/hilo/__main__.py
+++ hilo_template/hilo/__main__.py
@@ -3,3 +3,10 @@
 # *must write atleast two classes
 # *Each module, class and method must have a corresponding comment using the style demonstrated in the solo checkpoint.
 # *The game must remain generally true to the order of play described in the rules.
+#
+# other options
+#Enhanced input validation with user-friendly messages.
+#Enhanced game play and game over messages.
+#Enhanced card representation (incorporate Jack, Queen, King and Ace)
+#Enhanced game display (e.g. different colors for correct or incorrect guesses)
+#
