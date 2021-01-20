6203bc44d1820ce2ff2333e6ec39cccbf3405c21
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-01-20 14:29:16 -0700

    Update __main__.py

--- hilo_template/hilo/__main__.py
+++ hilo_template/hilo/__main__.py
@@ -1,1 +1,5 @@
+#Hilo team challenge 4
 # TODO: Add entry point code here
+# *must write atleast two classes
+# *Each module, class and method must have a corresponding comment using the style demonstrated in the solo checkpoint.
+# *The game must remain generally true to the order of play described in the rules.
