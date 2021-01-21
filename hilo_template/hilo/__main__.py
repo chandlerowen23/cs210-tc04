ef2b8110505a0329b9fdc9b99d12f2667520995c
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-01-21 11:40:50 -0700

    Update __main__.py

--- hilo_template/hilo/__main__.py
+++ hilo_template/hilo/__main__.py
@@ -10,76 +10,8 @@
 #Enhanced card representation (incorporate Jack, Queen, King and Ace)
 #Enhanced game display (e.g. different colors for correct or incorrect guesses)
 
-import random
+import game.comp_card as Cards
 
-class rand_card:
-    #creates random card
-
-    def __init__(self):
-        self.card = 0
-    
-    def next_card(self):
-        card = random.randint(1,13)
-        #print(f"The card is: {card}")
-        return card
-
-class comp_card:
-    #compares past card with new card
-
-    def __init__(self):
-        self.card1 = 0
-        self.card2 = 0
-        self.score = 300
-        self.rand = rand_card()
-        self.play = True
-
-
-    def start_game(self):
-        while (self.play):
-            if (self.card2 == 0):
-                self.card1 = self.rand.next_card()
-                print(f"The card is: {self.card1}")
-                x = input("Higher or lower? [h/l] ")
-                self.card2 = self.rand.next_card()
-                print(f"Next card was: {self.card2}")
-                if (x == "h") & (self.card1 < self.card2) | (x == "l") & (self.card1 > self.card2):
-                    self.score += 100
-                    print(f"Your score is: {self.score}")
-                    self.keep_play()
-                    #break
-                else:
-                    self.score -= 75
-                    print(f"Your score is: {self.score}")
-                    self.keep_play()
-                    #break
-            else:
-                self.card1 = self.card2
-                print(f"The card is: {self.card1}")
-                x = input("Higher or lower? [h/l] ")
-                self.card2 = self.rand.next_card()
-                print(f"Next card was: {self.card2}")
-                if (x == "h") & (self.card1 < self.card2) | (x == "l") & (self.card1 > self.card2):
-                    self.score += 100
-                    print(f"Your score is: {self.score}")
-                    self.keep_play()
-                    #break
-                else:
-                    self.score -= 75
-                    print(f"Your score is: {self.score}")
-                    self.keep_play()
-
-    def keep_play(self):
-        y = input("Keep playing? [y/n] ")
-        if (y == "y"):
-            self.play = True
-            print()
-        else:
-            self.play = False
-            print()
-            print(f"Your Final score is: {self.score}")
-
-
-
-hilo = comp_card()
+hilo = Cards()
 hilo.start_game()
 
